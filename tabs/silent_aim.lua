6eb22059ab32d762010b8ddff387f4e67ba63e30
abdullhx <116509406+abdullhx@users.noreply.github.com>
2026-02-07 05:27:50 +0300

    Update URLs for loading Silent Aim elements

--- tabs/silent_aim.lua
+++ tabs/silent_aim.lua
@@ -1,6 +1,6 @@
 return function(Tab)
     -- تحميل عناصر Silent Aim
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/USERNAME/REPO/main/elements/toggle_silent_aim.lua"))()(Tab)
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/USERNAME/REPO/main/elements/slider_fov.lua"))()(Tab)
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/USERNAME/REPO/main/elements/dropdown_ignore.lua"))()(Tab)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/abdullhx/PaidScript/refs/heads/main/elements/toggle_silent_aim.lua"))()(Tab)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/abdullhx/PaidScript/refs/heads/main/elements/slider_fov.lua"))()(Tab)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/abdullhx/PaidScript/refs/heads/main/elements/dropdown_ignore.lua"))()(Tab)
 end
