1f0bded2233fd5cc16d47d974e0f2fa1afe94a77
abdullhx <116509406+abdullhx@users.noreply.github.com>
2026-02-07 05:22:54 +0300

    Add Silent Aim tab with loading elements

---
+++ tabs/silent_aim.lua
@@ -0,0 +1,6 @@
+return function(Tab)
+    -- تحميل عناصر Silent Aim
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/USERNAME/REPO/main/elements/toggle_silent_aim.lua"))()(Tab)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/USERNAME/REPO/main/elements/slider_fov.lua"))()(Tab)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/USERNAME/REPO/main/elements/dropdown_ignore.lua"))()(Tab)
+end
