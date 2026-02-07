ab743826ab8d6c5709cb576a679c818f877297fe
abdullhx <116509406+abdullhx@users.noreply.github.com>
2026-02-07 05:35:20 +0300

    Adjust FOV slider range and default value

--- elements/slider_fov.lua
+++ elements/slider_fov.lua
@@ -1,12 +1,23 @@
 return function(Tab)
+    local Players = game:GetService("Players")
+    local LocalPlayer = Players.LocalPlayer
+    local pg = LocalPlayer:WaitForChild("PlayerGui")
+    local TargetGui = pg:FindFirstChild("TargetGui")
+    if not TargetGui then return end
+    local Circle = TargetGui:FindFirstChildWhichIsA("Frame")
+    if not Circle then return end
+
+    local FOV = 200
+
     Tab:CreateSlider({
         Name = "FOV",
-        Range = {10, 200},
+        Range = {50,200},
         Increment = 1,
-        CurrentValue = 70,
-        Color = Color3.fromRGB(0, 255, 0), -- أخضر
+        CurrentValue = FOV,
+        Color = Color3.fromRGB(0,255,0),
         Callback = function(Value)
-            print("FOV:", Value)
+            FOV = Value
+            Circle.Size = UDim2.new(0, FOV, 0, FOV)
         end
     })
 end
